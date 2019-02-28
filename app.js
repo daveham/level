ed2a29e2e4b4434b9ad6e97d5ad48ac7ec278879
Dave Hamilton <dave@call-em-all.com>
2019-02-28 17:01:05 -0600

    Applying json encoding for sublevels.

--- app.js
+++ app.js
@@ -5,16 +5,16 @@ const sub = require('subleveldown');
 const db = levelup(leveldown('./mydb'));
 
 const sliceOne = sub(db, 'sliceOne');
-const sliceOneNestOne = sub(sliceOne, 'nestOne');
+const sliceOneNestOne = sub(sliceOne, 'nestOne', {  valueEncoding: 'json' });
 
 const sliceTwo = sub(db, 'sliceTwo');
-const sliceTwoNestTwo = sub(sliceTwo, 'nextTwo');
+const sliceTwoNestTwo = sub(sliceTwo, 'nextTwo', {  valueEncoding: 'json' });
 
 db.put('root', 'rootValue')
-  .then(() => sliceOneNestOne.put('sliceOne', 'sliceOneValue'))
-  .then(() => sliceTwoNestTwo.put('sliceTwo', 'sliceTwoValue'))
+  .then(() => sliceOneNestOne.put('sliceOne', { label: 'one', data: { raw: 1, text: 'one' } }))
+  .then(() => sliceTwoNestTwo.put('sliceTwo', { label: 'two', data: { raw: 2, text: 'two' } }))
   .then(() => sliceOneNestOne.get('sliceOne'))
-  .then((value) => console.log('sliceOne: ' + value))
+  .then((value) => console.log('sliceOne: ' + JSON.stringify(value)))
   .then(() => sliceTwoNestTwo.get('sliceTwo'))
-  .then((value) => console.log('sliceTwo: ' + value))
+  .then((value) => console.log('sliceTwo: ' + JSON.stringify(value)))
   .then(() => console.log('finished'));
